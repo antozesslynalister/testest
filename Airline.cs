b8673f75106211766cc27ff47746e4effbb6cd71
antozesslynalister <S10270608@connect.np.edu.sg>
2025-01-20 14:46:48 +0800

    Add files via upload

---
+++ Airline.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg_assignment
+{
+    public class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+
+        public bool AddFlight(Flight flight)
+        {
+            if (!Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights[flight.FlightNumber] = flight;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveFlight(Flight flight)
+        {
+            return Flights.Remove(flight.FlightNumber);
+        }
+
+        public double CalculateFees()
+        {
+            double totalFees = 0;
+            foreach (var flight in Flights.Values)
+            {
+                totalFees += flight.CalculateFees();
+            }
+            return totalFees;
+        }
+
+        public override string ToString()
+        {
+            return $"Airline: {Name}, Code: {Code}, Flights: {Flights.Count}";
+        }
+    }
+
+}
